682190ddd046e1a25951987068354ca5649e1f4f
김나영 nayoung_kim <102217891+kimna0920@users.noreply.github.com>
2023-05-16 15:26:21 +0900

    Create ch2_8.c
    예제2_8

---
+++ ch2_8.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <dirent.h>
+
+int main(){
+    DIR *dp;
+    struct dirent *dent;
+    
+    dp = opendir(".");
+
+    while ((dent=readdir(dp))){
+        printf("Name : %s ", dent->d_name);
+        printf("Inode : %d\n", (int)dent->d_ino);
+    }
+    closedir(dp);
+}
